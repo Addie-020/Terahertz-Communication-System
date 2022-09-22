c2a381191690410dd20792c0463a0227bac6e4e2
Addie-020 <shenzhiyu111i@126.com>
2022-09-22 14:01:14 +0800

    New update

---
+++ 2_pc_sys_comm/1_uart_comm/version_2.0/SerialCommunication_RX_pro.m
@@ -0,0 +1,36 @@
+% Uart Receive Data
+
+s = serialport("COM13",9600);
+input("按回车键接收...");
+flush(s);
+M = 0;
+N = 0;
+capacity = 0;
+T = read(s,1,"uint16");
+if T==0
+    M = read(s,1,"uint16");
+    N = read(s,1,"uint16");
+    capacity = M.*N;
+    data = read(s,capacity,"uint16");
+%     write(s,0,"uint8");
+else
+%     write(s,1,"uint8");
+end
+
+%图像按宽高存储写入
+i=1;
+for k1=1:N
+    for k2=1:1:M
+          image1(k1,k2)=uint16(data(i));
+          i=i+1;
+    end
+end
+%屏幕输出部分
+figure;
+image2 = uint8(image1);
+imshow(image2);
+
+%保存文件输出
+[name,path]=uiputfile([path 'imggray.bmp'], '保存灰度图像');
+file=strcat(path,name);
+imwrite(image2,file,'bmp');
